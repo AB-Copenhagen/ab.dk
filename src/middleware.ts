a13cbf0f600158a455da130daf3b3aa84bf3bc51
felixwidjaja <fwidjaja@datum.net>
2026-07-25 22:08:14 +0700

    style: run prettier on middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -148,7 +148,8 @@ async function handleRequest(context: APIContext, next: MiddlewareNext) {
     '/en/play',
   ];
   const isGated = GATED_PATHS.some(
-    (p) => context.url.pathname === p || context.url.pathname.startsWith(`${p}/`)
+    (p) =>
+      context.url.pathname === p || context.url.pathname.startsWith(`${p}/`)
   );
 
   if (isGated) {
